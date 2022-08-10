4eb93c42d3637fdb5a102c68c3d7824bbd25165e
Bodo Schulz <bodo@boone-schulz.de>
2022-08-10 06:14:27 +0200

    add support for artix linux

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -31,17 +31,29 @@ def get_vars(host):
 
     """
     base_dir, molecule_dir = base_directory()
+    distribution = host.system_info.distribution
+    operation_system = None
 
-    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
-    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
-    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+    if distribution in ['debian', 'ubuntu']:
+        operation_system = "debian"
+    elif distribution in ['redhat', 'ol', 'centos', 'rocky', 'almalinux']:
+        operation_system = "redhat"
+    elif distribution in ['arch', 'artix']:
+        operation_system = f"{distribution}linux"
+
+    file_defaults = f"file={base_dir}/defaults/main.yaml name=role_defaults"
+    file_vars = f"file={base_dir}/vars/main.yaml name=role_vars"
+    file_molecule = f"file={molecule_dir}/group_vars/all/vars.yaml name=test_vars"
+    file_distibution = f"file={base_dir}/vars/{operation_system}.yaml name=role_distibution"
 
     defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
     vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    distibution_vars = host.ansible("include_vars", file_distibution).get("ansible_facts").get("role_distibution")
     molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
 
     ansible_vars = defaults_vars
     ansible_vars.update(vars_vars)
+    ansible_vars.update(distibution_vars)
     ansible_vars.update(molecule_vars)
 
     templar = Templar(loader=DataLoader(), variables=ansible_vars)
@@ -56,20 +68,17 @@ def test_packages(host):
     distribution = host.system_info.distribution
     release = host.system_info.release
 
-    pp.pprint(distribution)
-    pp.pprint(release)
+    print(f"distribution: {distribution}")
+    print(f"release     : {release}")
 
     packages = []
     packages.append("knot")
 
-    # if(distribution == 'arch'):
-    #     packages.append("knot")
-    # else:
-    #     packages.append("docker-ce")
-
-    for package in packages:
-        p = host.package(package)
-        assert p.is_installed
+    # artix ist not supported
+    if not distribution == "artix":
+        for package in packages:
+            p = host.package(package)
+            assert p.is_installed
 
 
 @pytest.mark.parametrize("dirs", [
@@ -86,3 +95,22 @@ def test_service_running_and_enabled(host):
     service = host.service('knot')
     assert service.is_running
     assert service.is_enabled
+
+
+def test_listening_socket(host, get_vars):
+    """
+    """
+    listening = host.socket.get_listening_sockets()
+
+    for i in listening:
+        print(i)
+
+    bind_address = "127.0.0.1"
+    bind_port = 5353
+
+    listen = []
+    listen.append(f"tcp://{bind_address}:{bind_port}")
+
+    for spec in listen:
+        socket = host.socket(spec)
+        assert socket.is_listening
